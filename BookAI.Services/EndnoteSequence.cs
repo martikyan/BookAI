e396eef522ecf7b4b6de8820e0a6cf9551424178
Kar Martikyan <kamartikyan@gmail.com>
2025-02-06 22:46:18 +0400

    Sequence fixes made

--- BookAI.Services/EndnoteSequence.cs
+++ BookAI.Services/EndnoteSequence.cs
@@ -1,12 +1,24 @@
+using System.Text;
+
 namespace BookAI.Services;
 
 public class EndnoteSequence
 {
-    private int _iterator;
+    private readonly char[] _symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    private int _iterator = 0;
 
     public string GetNext()
     {
-        var bytes = BitConverter.GetBytes(++_iterator);
-        return Convert.ToBase64String(bytes).TrimEnd('=').TrimEnd('A');
+        var sb = new StringBuilder();
+        var current = ++_iterator;
+        while (current > 0)
+        {
+            current--; // Adjust current before modulo
+            var charIndex = current % _symbols.Length;
+            current = current / _symbols.Length;
+            sb.Insert(0, _symbols[charIndex]);
+        }
+    
+        return sb.ToString();
     }
 }
